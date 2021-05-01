8387b374030c41f3a328b9120c8c55b2892c0775
Chris <75405216+Chabivz@users.noreply.github.com>
2021-05-01 09:23:50 -0700

    Revert "Super feature branch updated"

--- Assets/JS/script.js
+++
@@ -1,9 +0,0 @@
-var now = moment()
-var currentDayEl = $(".time");
-
-function displayDate() {
-    var date = now.format("dddd, MMMM Do YYYY, h:mm a");
-    currentDayEl.text(date);
-}
-
-displayDate();
