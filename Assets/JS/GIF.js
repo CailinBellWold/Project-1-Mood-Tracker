2c0396e6e3520add1dcb8e442a51db4c3e79ea41
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-05-03 21:39:55 -0700

    Angry, Happy, Sad, Calm Buttons Hooked Up. APIs all in 1 File.

--- Assets/JS/GIF.js
+++ Assets/JS/GIF.js
@@ -10,55 +10,35 @@ var happyBtn = document.querySelector("#happy");
 angryBtn.addEventListener('click',function (){
     
     var url = 'https://api.giphy.com/v1/gifs/search?api_key=c68nPrb4NwGEUnzP044FJ97Bl3RqTXi7&limit=30&rating=g&q=angry';
-     
-   
 
 fetch(url)
   .then(function (response) {
     return response.json();
   })
   .then(function (content) {
-    
     console.log(content.data);
-
-   
 
   for (var i = 0; i < content.data.length; i++) {
 
-    
     var img = document.querySelector("#content");
     var randomNum = Math.floor(Math.random() * content.data.length); 
     img.src = content.data[randomNum].images.downsized.url
     img.alt = content.data[randomNum].title
-
-    
   };
-
- 
-
-  
   });
-
-  
-
 });
 
 // function to generate happy GIF 
 happyBtn.addEventListener('click',function (){
   
   var url = 'https://api.giphy.com/v1/gifs/search?api_key=c68nPrb4NwGEUnzP044FJ97Bl3RqTXi7&limit=30&rating=g&q=happy';
-   
- 
 
 fetch(url)
-.then(function (response) {
-  return response.json();
-})
-.then(function (content) {
-  
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (content) {
   console.log(content.data);
-
- 
 
 for (var i = 0; i < content.data.length; i++) {
 
@@ -66,29 +46,15 @@ for (var i = 0; i < content.data.length; i++) {
     var randomNum = Math.floor(Math.random() * content.data.length); 
     img.src = content.data[randomNum].images.downsized.url
     img.alt = content.data[randomNum].title
-  
-
-  
 };
-
-
-
-
 });
 
-
-
 });
-
-
-
 
 // function to generate calm GIF 
 calmBtn.addEventListener('click',function (){
- 
+
   var url = 'https://api.giphy.com/v1/gifs/search?api_key=c68nPrb4NwGEUnzP044FJ97Bl3RqTXi7&limit=30&rating=g&q=calm';
-   
- 
 
 fetch(url)
 .then(function (response) {
@@ -97,8 +63,6 @@ fetch(url)
 .then(function (content) {
   
   console.log(content.data);
-
- 
 
 for (var i = 0; i < content.data.length; i++) {
 
@@ -106,15 +70,6 @@ for (var i = 0; i < content.data.length; i++) {
   var randomNum = Math.floor(Math.random() * content.data.length); 
   img.src = content.data[randomNum].images.downsized.url
   img.alt = content.data[randomNum].title
-
-  
 };
-
-
-
-
 });
-
-
-
 });
---
+++ Assets/JS/all-api-scripts.js
@@ -0,0 +1,182 @@
+//TO DO: 404 Responses, Get Next Button Set Up To Reset/Pull Current API
+
+// API Keys
+let giphyApiKey = 'c68nPrb4NwGEUnzP044FJ97Bl3RqTXi7';
+
+// API / Jokes
+var jokesPunURL = 'https://v2.jokeapi.dev/joke/Pun?type=single&?format=json&?blacklistFlags=nsfw,religious,political,racist+,sexist,explicit&safe-mode';
+var jokesJokeURL = 'https://v2.jokeapi.dev/joke/Any?type=single&?format=json&?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&safe-mode';
+
+// API / GIPHY
+var giphySadURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + giphyApiKey + '&limit=30&rating=g&q=sad';
+let giphyAngryURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + giphyApiKey + '&limit=30&rating=g&q=angry';
+let giphyHappyURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + giphyApiKey + '&limit=30&rating=g&q=happy';
+let giphyCalmURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + giphyApiKey + '&limit=30&rating=g&q=calm';
+
+// API / Quotes
+let quoteCalmURL = 'https://favqs.com/api/qotd';
+
+// Buttons
+let angryBtn = document.querySelector("#angry");
+let sadBtn = document.querySelector("#sad");
+let calmBtn = document.querySelector("#calm");
+let happyBtn = document.querySelector("#happy");
+
+// Event Listeners
+angryBtn.addEventListener('click', getAngryResponse);
+happyBtn.addEventListener('click', getHappyResponse);
+sadBtn.addEventListener('click', getSadResponse);
+calmBtn.addEventListener('click', getCalmResponse);
+
+// Modal
+let textInput = $(".text-one");
+let moduleActive = $(".modal")
+let closeModal = $(".modal-close");
+let nextBtn = $("#next");
+
+// Angry Response
+function getAngryResponse() {
+    fetch(jokesJokeURL, {
+        method: 'GET', 
+        credentials: 'same-origin', 
+        redirect: 'follow', 
+    })
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+        var joke = data.joke;
+        $("#content").append("<h3>" + joke + "<h3>");
+        moduleActive.addClass("is-active"); 
+    })
+
+    fetch(giphyAngryURL, {
+        method: 'GET',
+        credentials: 'same-origin',
+        redirect: 'follow',
+    })
+    .then(function (response) {
+    return response.json();
+    })
+
+    .then(function (content) {
+        var randomNum = Math.floor(Math.random() * content.data.length); 
+        var img = content.data[randomNum].images.downsized.url;
+        var alt = content.data[randomNum].title
+        $("#content").append("<img src=" + img + "alt=" + alt + ">");
+    })
+}; 
+
+// Happy Response
+function getHappyResponse() {
+    fetch(jokesJokeURL, {
+        method: 'GET', 
+        credentials: 'same-origin', 
+        redirect: 'follow', 
+    })
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+        var joke = data.joke;
+        $("#content").append("<h3>" + joke + "<h3>");
+        moduleActive.addClass("is-active"); 
+    })
+
+    fetch(giphyHappyURL, {
+        method: 'GET',
+        credentials: 'same-origin',
+        redirect: 'follow',
+    })
+    .then(function (response) {
+    return response.json();
+    })
+
+    .then(function (content) {
+        var randomNum = Math.floor(Math.random() * content.data.length); 
+        var img = content.data[randomNum].images.downsized.url;
+        var alt = content.data[randomNum].title
+        $("#content").append("<img src=" + img + "alt=" + alt + ">");
+    })
+}; 
+
+// Sad Response
+function getSadResponse() {
+    fetch(jokesPunURL, {
+        method: 'GET', 
+        credentials: 'same-origin', 
+        redirect: 'follow', 
+    })
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+        var joke = data.joke;
+        $("#content").append("<h3>" + joke + "<h3>");
+        moduleActive.addClass("is-active"); 
+    })
+
+    fetch(giphySadURL, {
+        method: 'GET',
+        credentials: 'same-origin',
+        redirect: 'follow',
+    })
+    .then(function (response) {
+    return response.json();
+    })
+
+    .then(function (content) {
+        var randomNum = Math.floor(Math.random() * content.data.length); 
+        var img = content.data[randomNum].images.downsized.url;
+        var alt = content.data[randomNum].title
+        $("#content").append("<img src=" + img + "alt=" + alt + ">");
+    })
+}; 
+
+// Calm Reponse
+function getCalmResponse() {
+    fetch(quoteCalmURL, {
+        method: 'GET', 
+        credentials: 'same-origin', 
+        redirect: 'follow', 
+    })
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+        var bodyQuote = data.quote.body;
+        var author = data.quote.author;
+        $("#content").append("<h3>" + '"' + bodyQuote + '"' + "<h3>");
+        $("#content").append("<h4>" + "-" + author + "<h4>");
+        moduleActive.addClass("is-active"); 
+    })
+
+    fetch(giphyCalmURL, {
+        method: 'GET',
+        credentials: 'same-origin',
+        redirect: 'follow',
+    })
+    .then(function (response) {
+    return response.json();
+    })
+
+    .then(function (content) {
+        var randomNum = Math.floor(Math.random() * content.data.length); 
+        var img = content.data[randomNum].images.downsized.url;
+        var alt = content.data[randomNum].title
+        $("#content").append("<img src=" + img + "alt=" + alt + ">");
+    })
+}; 
+
+    // Next Button - piles new ones on top of previous Event Listener
+    nextBtn.on("click", getSadResponse); // I think we can set a reset that is a 2-part (reset, then run appropriate emotion response)
+
+// Close Modal function Event Listener
+closeModal.on("click", function() {
+    moduleActive.removeClass("is-active");
+    location.reload();
+});
+
+
+
+
