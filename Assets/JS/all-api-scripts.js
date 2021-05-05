94ba59b7850abe714fb06075a254860cd11d44e8
Tiffany Washke <twashke@gmail.com>
2021-05-04 22:13:10 -0700

    updated next button and removed from functions

--- Assets/JS/all-api-scripts.js
+++ Assets/JS/all-api-scripts.js
@@ -1,5 +1,4 @@
 //TO DO: 404 Responses, Get Next Button Set Up To Reset/Pull Current API, Filter for Cigarette and Booze content.
-
 // API Keys
 let giphyApiKey = 'c68nPrb4NwGEUnzP044FJ97Bl3RqTXi7';
 
